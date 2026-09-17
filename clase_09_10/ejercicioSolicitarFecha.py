5d10d3d857da4bf88ca4611b4def913454f6fdff
Diego <diegopenna@gmail.com>
2026-09-17 20:42:09 -0300

    Fin del Ejercicio solicitarfecha.

--- clase_09_10/ejercicioSolicitarFecha.py
+++ clase_09_10/ejercicioSolicitarFecha.py
@@ -1,5 +1,5 @@
 def solictarFecha(mensaje = "Ingrese Fecha (dd/mm/aaaa): "):
-    valDias = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    valDias = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     while True:
         fecha = input(mensaje)
         #validar que el formato sea ##/##/####
@@ -28,16 +28,47 @@ def solictarFecha(mensaje = "Ingrese Fecha (dd/mm/aaaa): "):
             print("Formato de fecha incorrecto. Número de mes incorrecto.")
             continue
 
+        if esAnioBisiesto2(numAnio):
+            valDias[1] = 29
+        else:
+            valDias[1] = 28
+
         if not (1 <= numDia <= valDias[numMes - 1]):
             print("Formato de fecha incorrecto. Número de dia incorrecto.")
             continue
 
-        
-        
+# Otra opcion para validar el dia sin la lista de validacion
+#        if numMes in [1, 3, 5, 7, 8, 10, 12]:
+#            if not (1 <= numDia <= 31):
+#                print("Formato de fecha incorrecto. Número de dia incorrecto.")
+#                continue
+#        elif numMes in [4, 6, 9, 11]:
+#            if not (1 <= numDia <= 30):
+#                print("Formato de fecha incorrecto. Número de dia incorrecto.")
+#                continue
+
 
         break
 
     return f"{dia}/{mes}/{anio}"
 
+
+def esAnioBisiesto(anio):
+    #Tener en cuenta que un año es bisiesto si es divisible por 4, 
+    #    excepto los años divisibles por 100, 
+    #    que solo son bisiestos si además son divisibles por 400.
+    if anio % 4 == 0:
+        if anio % 100 == 0:
+            return anio % 400 == 0
+        else:
+            return True
+    else:
+        return False
+
+def esAnioBisiesto2(anio):
+    if anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
+        return True
+    else:
+        return False
 
 print(solictarFecha())
